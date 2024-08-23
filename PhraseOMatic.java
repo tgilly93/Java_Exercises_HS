453e0c408d81f7add2dcdb7d5197a19a3154136b
Terry Gilmore <tgilly93@gmail.com>
2024-08-23 12:25:56 -0400

    TG first Java push

---
+++ PhraseOMatic.java
@@ -0,0 +1,62 @@
+import java.util.Random;
+
+public class PhraseOMatic {
+    public static void main(String[] args) {
+        String[] wordListOne = {
+            "agnostic",
+             "opinionated",
+              "voice activated",
+               "haptically driven",
+                "extensible",
+                 "reactive",
+                  "agent based",
+                   "functional",
+                    "AI enabled",
+                     "strongly typed"
+        };
+
+        String[] wordListTwo = {
+            "loosely coupled",
+            "six sigma",
+            "asynchronous",
+            "event-driven",
+            "pub-sub",
+            "IoT",
+            "cloud-native",
+            "service oriented",
+            "containerized",
+            "serverless",
+            "microservices",
+            "distributed ledger"
+        };
+
+        String[] wordListThree = {
+            "framework",
+            "library",
+            "DSL",
+            "REST API",
+            "repository",
+            "pipeline",
+            "service mesh",
+            "architecture",
+            "perspective",
+            "design",
+            "orientation"
+    };
+
+    int oneLength = wordListOne.length;
+    int twoLength = wordListTwo.length;
+    int threeLength = wordListThree.length;
+
+    Random randomGenerator = new Random();
+    int rand1 = randomGenerator.nextInt(oneLength);
+    int rand2 = randomGenerator.nextInt(twoLength);
+    int rand3 = randomGenerator.nextInt(threeLength);
+
+    String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+
+    System.out.println(
+        "What we need is a " + phrase
+    );
+}
+}
